46d22991334cbd6f50d02681bc1cb44cd6720eb4
Leffernan <159530480+Leffernan@users.noreply.github.com>
2024-02-09 11:10:42 +0100

    Initial commit of quiz module files

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function checkAnswer(selectedOption) {
+  const correctAnswer = "A"; // Dette bør endres basert på det aktuelle spørsmålet
+
+  if (selectedOption === correctAnswer) {
+    alert("Riktig svar!");
+  } else {
+    alert("Feil svar, prøv igjen!");
+  }
+}
